dfd118190d177554ddba2b1c042d8fb86ec17523
clemburg <clemburg>
2000-02-24 22:50:43 +0000

    separated to_string(), get_message(), and stacktrace()

--- src/Test-Unit/lib/Test/Unit/Exception.pm
+++ src/Test-Unit/lib/Test/Unit/Exception.pm
@@ -6,23 +6,34 @@ sub new {
     my $class = shift;
     my ($message) = @_;
     
+    $message = '' unless defined($message);
+    $message = ref($class) . ": " . $message . "\n";
+
     my $i = 0;
     my $stacktrace = '';
     my ($pack, $file, $line, $subname, $hasargs, $wantarray);
     
-    $message = '' unless defined($message);
-    $stacktrace = ref($class) . ": " . $message . "\n";
     while (($pack, $file, $line, $subname, 
 	    $hasargs, $wantarray) = caller($i++)) {
 	$stacktrace .= "Level $i: in package '$pack', file '$file', at line '$line', sub '$subname'\n";
     }
     
-    bless { stacktrace => $stacktrace }, $class;
+    bless { _message => $message, _stacktrace => $stacktrace }, $class;
 }
 
 sub stacktrace {
     my $self = shift;
-    return $self->{stacktrace};
+    return $self->{_stacktrace};
+}
+
+sub get_message {
+    my $self = shift;
+    return $self->{_message};
+}
+
+sub to_string {
+    my $self = shift;
+    return $self->get_message() . $self->stacktrace();
 }
 
 1;
