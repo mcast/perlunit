10d63a032ae63ee9c62e146868af66d558cc679c
clemburg <clemburg>
2000-02-29 23:18:20 +0000

    first start at some documentation

--- src/Test-Unit/lib/Test/Unit/TestCase.pm
+++ src/Test-Unit/lib/Test/Unit/TestCase.pm
@@ -90,3 +90,104 @@ sub to_string {
 }
 
 1;
+
+=head1 NAME
+
+Test::Unit::TestCase - unit testing framework testcase base class
+
+=head1 SYNOPSIS
+
+  package FooBar;
+  use base qw(Test::Unit::TestCase);
+  
+  sub set_up {
+      # provide fixture
+  }
+  sub tear_down {
+      # clean up after test
+  }
+  sub test_foo {
+      # test the foo feature
+  }
+  sub test_bar {
+      # test the bar feature
+  }
+
+=head1 DESCRIPTION
+
+(Taken from the JUnit TestCase class documentation)
+
+A test case defines the fixture to run multiple tests. To define a test case
+
+=item 1) implement a subclass of TestCase
+
+
+=item 2) define instance variables that store the state of the fixture
+
+
+=item 3) initialize the fixture state by overriding set_up()
+
+
+=item 4) clean-up after a test by overriding tear_down().
+
+=back
+
+Each test runs in its own fixture so there can be no side effects among 
+test runs. Here is an example:
+
+  package MathTest;
+  use base qw(Test::Unit::TestCase);
+
+  sub new {
+      my $class = shift;
+      my ($name) = @_;
+      my $self = bless {}, $class;
+      my $a_test_case = $self->SUPER::new($name);
+      bless $a_test_case, $class;
+      $a_test_case->{_value_1} = 0;
+      $a_test_case->{_value_2} = 0;
+      return $a_test_case;
+  }
+  
+  sub set_up {
+      my $self = shift;
+      $self->{_value_1} = 2;
+      $self->{_value_2} = 3;
+  }
+  
+For each test implement a method which interacts with the fixture. Verify
+the expected results with assertions specified by calling $self->assert()
+with a boolean value.
+
+  sub test_add {
+      my $self = shift;
+      my $result = $self->{_value_1} + $self->{_value_2};
+      $self->assert($result == 5);
+  }
+  
+Once the methods are defined you can run them. The normal way to do
+this uses reflection to implement run_test. It dynamically finds and
+invokes a method. For this the name of the test case has to correspond
+to the test method to be run. The tests to be run can be collected into
+a TestSuite. The framework provides different test runners, which can
+run a test suite and collect the results. A test runner either expects
+a method suite() as the entry point to get a test to run or it will
+extract the suite automatically.
+
+=head1 AUTHOR
+
+Framework JUnit authored by Kent Beck and Erich Gamma.
+    
+Ported from Java to Perl by Christian Lemburg, <lemburg@acm.org>.
+
+Copyright (c) 2000 Christian Lemburg. All rights reserved. 
+This program is free software; you can redistribute it and/or modify 
+it under the same terms as Perl itself.   
+
+=head1 SEE ALSO
+
+Test::Unit::Tutorial, module documentation for TestSuite, TestRunner,
+TkTestRunner. For further examples, take a look at the framework self
+test collection (Test::Unit::tests::AllTests). 
+
+=cut
