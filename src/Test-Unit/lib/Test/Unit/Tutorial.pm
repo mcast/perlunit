505b3352296932b8219e6f2b22516f24ee7edf77
clemburg <clemburg>
2000-03-05 09:22:03 +0000

    container for tutorial on unit testing with framework

---
+++ src/Test-Unit/lib/Test/Unit/Tutorial.pm
@@ -0,0 +1,38 @@
+package Test::Unit::Tutorial;
+
+# this is only a container for the Test::Unit tutorial
+# to allow viewing tutorial-style documentation on
+# unit testing by way of the perldoc utility.
+
+1;
+__END__
+# Below is the stub of documentation for your module. You better edit it!
+
+=head1 NAME
+
+Test::Unit::Tutorial - Tutorial on unit testing
+
+=head1 SYNOPSIS
+
+perldoc Test::Unit::Tutorial
+
+=head1 DESCRIPTION
+
+Here should be extensive documentation on what unit testing is, why it
+is useful, and how to do it with the Test::Unit collection of modules.
+
+Sorry for not implementing this yet.
+
+=head1 AUTHOR
+
+Christian Lemburg <lemburg@acm.org>
+
+=head1 SEE ALSO
+
+=item 1) The module documentation for all modules in the Test::Unit tree.
+
+=item 2) Refactoring. Improving The Design Of Existing Code. Martin Fowler. Addison-Wesley, 1999.
+
+=item 3) http://www.xProgramming.com/
+
+=cut
