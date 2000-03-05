f8f79bebff56231cc504150e605d2deb3f519bf5
clemburg <clemburg>
2000-03-05 08:53:24 +0000

    added tests for procedural API test suites composing feature add_suite()

--- src/Test-Unit/lib/Test/Unit/tests/TestUnitTest.pm
+++ src/Test-Unit/lib/Test/Unit/tests/TestUnitTest.pm
@@ -116,4 +116,55 @@ EOT
     $self->assert(-s $self->{_my_tmpfile}); # visible output
 }
 
+sub test_adding_suites_to_default_package {
+    my $self = shift;
+    my $filename = $self->{_my_tmpfile};
+    eval << "EOT";
+package To_Add_Fails;
+use Test::Unit;
+sub test_1 { assert(23 == 42); }
+sub test_2 { assert(42 == 23); }
+package To_Add_To; 
+use Test::Unit;
+sub test_1 { assert(42 == 42); }
+sub test_2 { assert(23 == 23); }
+create_suite();
+create_suite("To_Add_Fails"); 
+add_suite("To_Add_Fails");
+open(FH, '>$filename');
+run_suite(undef, \*FH);
+close(FH);
+EOT
+    # this depends on the die message in Test::Unit::TestRunner
+    $self->assert($@ eq "\nTest was not successful.\n"); # exit status
+    $self->assert(-s $self->{_my_tmpfile}); # visible output
+}
+
+sub test_adding_suites_to_explicit_package {
+    my $self = shift;
+    my $filename = $self->{_my_tmpfile};
+    eval << "EOT";
+package To_Add_Fails_Too;
+use Test::Unit;
+sub test_1 { assert(23 == 42); }
+sub test_2 { assert(42 == 23); }
+package To_Add_To_Explicitly; 
+use Test::Unit;
+sub test_1 { assert(42 == 42); }
+sub test_2 { assert(23 == 23); }
+package A_Third_Package;
+use Test::Unit;
+create_suite("To_Add_To_Explicitly");
+create_suite("To_Add_Fails_Too"); 
+add_suite("To_Add_Fails_Too", "To_Add_To_Explicitly");
+open(FH, '>$filename');
+run_suite("To_Add_To_Explicitly", \*FH);
+close(FH);
+EOT
+    # this depends on the die message in Test::Unit::TestRunner
+    $self->assert($@ eq "\nTest was not successful.\n"); # exit status
+    $self->assert(-s $self->{_my_tmpfile}); # visible output
+}
+
+
 1;
