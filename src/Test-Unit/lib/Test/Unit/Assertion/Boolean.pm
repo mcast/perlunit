cb3b1ed224a2ad68549ce18747b1cc14a530e136
pdcawley <pdcawley>
2001-03-05 18:44:48 +0000

    Added a bunch of documentation to T::U::Assertion and its subclasses.
    Removed the dependency on B::Deparse (I think. If someone who hasn't got
    this installed could test it...)

--- src/Test-Unit/lib/Test/Unit/Assertion/Boolean.pm
+++ src/Test-Unit/lib/Test/Unit/Assertion/Boolean.pm
@@ -17,3 +17,33 @@ sub do_assertion {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Test::Unit::Assertion::Boolean - A boolean assertion
+
+=head1 SYNOPSIS
+
+Pay no attention to the man behind the curtain. This is simply a
+boolean assertion that exists solely to rationalize the way
+Test::Unit::Assert::assert does its thing. You should never have to
+instantiate one of these directly. Ever. Go away. There's nothing to
+see here.
+
+
+=head1 AUTHOR
+
+Copyright (c) 2001 Piers Cawley E<lt>pdcawley@iterative-software.comE<gt>.
+
+All rights reserved. This program is free software; you can
+redistribute it and/or modify it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+Look, I've told you, there's nothing going on here. If you go looking
+at the listing of this module you'll see that it does almost nothing.
+Why on earth you're still reading at this point is something of a
+mystery to me. After all, if you're hacking on the Test::Unit source
+code you'll be able to use the Source.
