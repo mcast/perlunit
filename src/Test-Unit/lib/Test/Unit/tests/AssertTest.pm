facb3067e0238158f2bcf7ab2e618df626a8fa67
pdcawley <pdcawley>
2001-09-02 11:08:38 +0000

    Added a few more tests of Assertions, testing assert_equals. 'assert_equals' bases its choice of whether to do numeric or string based assertions on the type of its first (not $self) argument.
    Note too the 'new' test matrix approach. I think that this might prove
    to be a useful technique for pulling into the framework proper, call it
    Test::Unit::TestResultsMatrix or something.

--- src/Test-Unit/lib/Test/Unit/tests/AssertTest.pm
+++ src/Test-Unit/lib/Test/Unit/tests/AssertTest.pm
@@ -9,6 +9,7 @@ use strict;
 use vars qw/@ISA/;
 
 use Error qw/:try/;
+use Class::Inner;
 
 package TestObject;
 sub new {
@@ -74,6 +75,97 @@ sub test_succeed_assert_not_null {
     $self->assert_not_null('undef');
     $self->assert_not_null(0);
     $self->assert_not_null(10);
+}
+
+sub test_assert_equals_again {
+    my $self = shift;
+    $self->assert_equals(1,'1.0', "1 == '1.0'");
+    $self->assert_equals('1.0', 1, "'1.0' == 1");
+    $self->assert_equals('foo', 'foo', 'foo eq foo');
+    $self->assert_equals('0e0', 0, '0E0 == 0');
+    $self->assert_equals(0, 'foo', "0 == 'foo'");
+    $self->assert_equals('0', 'foo', "'0' == 'foo'");
+}
+
+# Key = assert_method
+# Value = [[@arg_list],undef/expected exception]
+my %test_hash =
+(
+ assert_equals => {success => [
+                           {args => [0,'foo'],      name => "0 == 'foo'"},
+                           {args => [1,'1.0'],      name => "1 == '1.0'"},
+                           {args => ['1.0', 1],     name => "'1.0' == 1"},
+                           {args => ['foo', 'foo'], name => 'foo eq foo'},
+                           {args => ['0e0', 0],     name => '0E0 == 0'  },
+                           {args => [0, 'foo'],     name => "0 == 'foo'"},
+                              ],
+                   Test::Unit::ExceptionFailure =>
+                              [
+                           {args => [1,'foo'],      name => "1 != 'foo'"    },
+                           {args => ['foo', 0],     name => "'foo' ne 0"    },
+                           {args => ['foo', 1],     name => "'foo' ne 1"    },
+                           {args => [0,1],          name => "0 != 1"        },
+                           {args => ['foo', 'bar'], name => "'foo' ne 'bar'"},
+                              ],
+                  },
+);
+sub test_assert_not_equals {
+    my $self = shift;
+    foreach my $pair ([1,'foo'], ['foo', 0], ['foo', 1], [0,1], ['foo', 'bar']) {
+        try { $self->assert_equals(@$pair); 0}
+        catch Test::Unit::ExceptionFailure with { 1 } or
+            Test::Unit::ExceptionFailure->throw(-text => 'should not have matched',
+                                                -object => $self);
+    }
+}
+
+
+sub suite {
+    my $self = shift;
+    my $suite = Test::Unit::TestSuite->empty_new("Assertion Tests");
+    foreach my $test ($self->make_tests_from_matrix(\%test_hash)) {
+        $suite->add_test($test);
+    }
+    foreach my $test ($self->list_tests) {
+        no strict 'refs';
+        $suite->add_test($self->make_test_from_coderef(sub {my $self = shift; $self->$test(@_)},$test));
+    }
+    return $suite;
+}
+
+
+sub make_tests_from_matrix {
+    my $self = shift;
+    my $matrix = shift;
+    my @tests;
+    foreach my $method_name (keys %$matrix) {
+        # Build 'successful' tests.
+        foreach my $spec (@{$matrix->{$method_name}{success}}) {
+            push @tests, $self->make_test_from_coderef
+                (sub {
+                     my $self = shift;
+                     $self->$method_name(@{$spec->{args}});
+                 }, $spec->{name});
+        }
+        
+        foreach my $outcome (grep {$_ ne 'success'} keys %{$matrix->{$method_name}}) {
+            foreach my $spec (@{$matrix->{$method_name}{$outcome}}) {
+                push @tests, $self->make_test_from_coderef
+                    (sub {
+                         my $self = shift;
+                         try {
+                             $self->$method_name(@{$spec->{args}});
+                             0;
+                         }
+                         catch $outcome with {
+                              1;
+                         } or Test::Unit::ExceptionFailure->throw(-text => $spec->{name},
+                                                                  -object => $self);
+                     }, $spec->{name});
+            }
+        }
+    }
+    return @tests;
 }
 
 1;
