e3edd14d3d76024d71dffb74e225e744b0400c00
pdcawley <pdcawley>
2001-09-07 16:19:31 +0000

    An attempt at improving the documentation.

--- src/Test-Unit/lib/Test/Unit/TestCase.pm
+++ src/Test-Unit/lib/Test/Unit/TestCase.pm
@@ -38,7 +38,7 @@ sub run {
     my $self = shift;
     print ref($self) . "::run() called\n" if DEBUG;
     my ($result) = @_;
-    $result = create_result() unless defined($result);
+    $result ||= create_result();
     $result->run($self);
     return $result;
 }
@@ -51,6 +51,7 @@ sub run_bare {
         $self->run_test();
     }
     catch Error::Simple with {
+        # Something died, which throws an Error::Simple
         Test::Unit::Error->make_from_error_simple(shift, $self)->throw;
     }
     finally {
@@ -137,13 +138,34 @@ Test::Unit::TestCase - unit testing framework base class
         # clean up after test
     }
     sub test_foo {
-        # test the foo feature
+        my $self = shift;
+        my $obj = ClassUnderTest->new(...);
+        $self->assert_not_null($obj);
+        $self->assert_equals('expected result', $obj->foo);
+        $self->assert(qr/pattern/, $obj->foobar);
     }
     sub test_bar {
         # test the bar feature
     }
 
 =head1 DESCRIPTION
+
+Test::Unit::TestCase is the 'workhorse' of the PerlUnit framework.
+When writing tests, you generally subclass Test::Unit::TestCase, write
+C<set_up> and C<tear_down> functions if you need them, a bunch of
+C<test_*> test methods, then do
+
+    $ TestRunner.pl My::TestCase::Class
+
+and watch as your tests fail/succeed one after another. Or, if you
+want your tests to work under Test::Harness and the standard perlish
+'make test', you'd write a t/foo.t that looked like:
+
+    use Test::Unit::HarnessUnit;
+    my $r = Test::Unit::HarnessUnit->new();
+    $r->start('My::TestCase::Class');
+
+=head2 How To Use Test::Unit::TestCase
 
 (Taken from the JUnit TestCase class documentation)
 
@@ -158,7 +180,8 @@ implement a subclass of TestCase
 
 =item 2
 
-define instance variables that store the state of the fixture
+define instance variables that store the state of the fixture (I
+suppose if you are using Class::MethodMaker this is possible...)
 
 =item 3
 
@@ -170,23 +193,29 @@ clean-up after a test by overriding C<tear_down()>.
 
 =back
 
-Each test runs in its own fixture so there can be no side
+
+Implement your tests as methods. By default, all methods that match
+the regex C</^test/> are taken to be test methods. Note that, by
+default all the tests defined in the current class and all of its
+parent classes will be run. 
+
+By default, each test runs in its own fixture so there can be no side
 effects among test runs. Here is an example:
 
       package MathTest;
       use base qw(Test::Unit::TestCase);
 
       sub new {
-	  my $self = shift()->SUPER::new(@_);
-	  $self->{value_1} = 0;
-	  $self->{value_2} = 0;
-	  return $self;
+          my $self = shift()->SUPER::new(@_);
+	      $self->{value_1} = 0;
+	      $self->{value_2} = 0;
+	      return $self;
       }
 
       sub set_up {
-	  my $self = shift;
-	  $self->{value_1} = 2;
-	  $self->{value_2} = 3;
+	      my $self = shift;
+	      $self->{value_1} = 2;
+	      $self->{value_2} = 3;
       }
 
 For each test implement a method which interacts with the fixture.
@@ -194,9 +223,9 @@ Verify the expected results with assertions specified by calling
 C<$self-E<gt>assert()> with a boolean value.
 
       sub test_add {
-	  my $self = shift;
-	  my $result = $self->{value_1} + $self->{value_2};
-	  $self->assert($result == 5);
+	      my $self = shift;
+	      my $result = $self->{value_1} + $self->{value_2};
+	      $self->assert($result == 5);
       }
 
 Once the methods are defined you can run them. The normal way to do
@@ -208,9 +237,31 @@ runners, which can run a test suite and collect the results. A test
 runner either expects a method C<suite()> as the entry point to get a
 test to run or it will extract the suite automatically.
 
-If you do not like the rather verbose backtrace that appears when a
-test fails, you can use the C<quell_backtrace()> method. You will get
-any message provided, but not the backtrace.
+=head2 Writing Test Methods
+
+The return value of your test method is completely irrelevant. The
+various test runners assume that a test is executed successfully if no
+exceptions are thrown. Generally, you will not have to deal directly
+with exceptions, but will write tests that look something like:
+
+    sub test_something {
+        my $self = shift;
+        # Execute some code which gives some results.
+        ...
+        # Make assertions about those results
+        $self->assert_equals('expected value', $resultA);
+        $self->assert_not_null($result_object);
+        $self->assert(qr/some_pattern/, $resultB);
+    }
+
+The assert methods throw appropriate exceptions when the assertions fail, 
+which will generally stringify nicely to give you sensible error reports.
+
+L<Test::Unit::Assert> has more details on the various different
+C<assert> methods.
+
+L<Test::Unit::Exception> describes the Exceptions used within the
+C<Test::Unit::*> framework.
 
 =head2 Helper methods
 
@@ -238,6 +289,51 @@ provide a couple of null methods. Override them if you need to.
 
 =back
 
+=head2 How it All Works
+
+The PerlUnit framework is achingly complex. The basic idea is that you
+get to write your tests independently of the manner in which they will
+be run, either via a C<make test> type script, or through one of the
+provided TestRunners, the framework will handle all that for you. And
+it does. So for the purposes of someone writing tests, in the majority
+of cases the answer is 'It just does.'.
+
+Of course, if you're trying to extend the framework, life gets a
+little more tricky. The core class that you should try and grok is
+probably Test::Unit::Result, which, in tandem with whichever
+TestRunner is being used mediates the process of running tests,
+stashes the results and generally sits at the centre of everything.
+
+Better docs will be forthcoming.
+
+=head1 NOTES
+
+Here's a few things to remember when you're writing your test suite
+
+=over 4
+
+=item 1
+
+Tests are run in 'random' order; the list of tests in your TestCase
+are generated automagically from its symbol table, which is a hash, so
+methods aren't sorted there. 
+
+If you need to specify the test order you should either override the
+C<'list_tests'> method to return an ordered list of methodnames, or
+provide a C<'suite'> method, which returns a Test::Unit::TestSuite.
+
+However, even if you do manage to specify the test order, be careful,
+object data will not be retained from one test to another, if you want
+to use persistent data you'll have to use package lexicals or globals.
+(Yes, this is probably a bug).
+
+=back
+
+=head1 BUGS
+
+See note 1 for at least one bug that's got me scratching my head.
+There's bound to be others.
+
 =head1 AUTHOR
 
 Framework JUnit authored by Kent Beck and Erich Gamma.
@@ -260,6 +356,14 @@ More changes made by Piers Cawley <pdcawley@iterative-software.com>
 =head1 SEE ALSO
 
 =over 4
+
+=item *
+
+L<Test::Unit::Assert>
+
+=item *
+
+L<Test::Unit::Exception>
 
 =item *
 
