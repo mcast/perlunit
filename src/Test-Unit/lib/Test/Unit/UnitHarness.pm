a99a6f525d3f6be911da2d6ea68cd5138b86df70
adamspiers <adamspiers>
2001-11-29 18:17:17 +0000

    - @ISA = ('Exporter') line was overriding use base lines - whitespace now matches conventions we decided on

--- src/Test-Unit/lib/Test/Unit/UnitHarness.pm
+++ src/Test-Unit/lib/Test/Unit/UnitHarness.pm
@@ -3,8 +3,7 @@
 package Test::Unit::UnitHarness;
 
 BEGIN {require 5.002;}
-use base qw(Test::Unit::Runner Test::Unit::Test);
-use Exporter;
+use base qw(Test::Unit::Runner Test::Unit::Test Exporter);
 use Config;
 use Carp;
 use FileHandle;
@@ -17,14 +16,13 @@ use Test::Unit::Exception;
 use strict;
 
 use vars qw($VERSION $verbose $switches $have_devel_corestack $curtest
-	    @ISA @EXPORT @EXPORT_OK);
+            @EXPORT @EXPORT_OK);
 $have_devel_corestack = 0;
 
 $VERSION = "1.1502";
 
-@ISA=('Exporter');
-@EXPORT= qw(&runtests);
-@EXPORT_OK= qw($verbose $switches);
+@EXPORT = qw(&runtests);
+@EXPORT_OK = qw($verbose $switches);
 
 $verbose = 0;
 $switches = "-w";
@@ -37,9 +35,9 @@ sub new {
     
     my @_Tests = ();
     my $self = {
-	_Tests => \@_Tests,
-	_Name => $name,
-	_Names => [],
+        _Tests => \@_Tests,
+        _Name => $name,
+        _Names => [],
     };
     bless $self, $class;
     print ref($self) . "::new($name) called\n" if DEBUG;
@@ -48,76 +46,78 @@ sub new {
 }
 
 sub run {
-  my $self=shift;
-  my $result=shift;
-  my $test=$self->{_Name};
-  my $fh = new FileHandle;
-  my $next=1;
-  my $max=0;
-  my $message="";
+    my $self = shift;
+    my $result = shift;
+    my $test = $self->{_Name};
+    my $fh = new FileHandle;
+    my $next = 1;
+    my $max = 0;
+    my $message = "";
 
-  # pass -I flags to children
-  my $old5lib = $ENV{PERL5LIB};
-  local($ENV{'PERL5LIB'}) = join($Config{path_sep}, @INC);
+    # pass -I flags to children
+    my $old5lib = $ENV{PERL5LIB};
+    local($ENV{'PERL5LIB'}) = join($Config{path_sep}, @INC);
   
-  if ($^O eq 'VMS') { $switches =~ s/-(\S*[A-Z]\S*)/"-$1"/g }
+    if ($^O eq 'VMS') { $switches =~ s/-(\S*[A-Z]\S*)/"-$1"/g }
 
-  $fh->open($test) or print "can't open $test. $!\n";
-  my $first = <$fh>;
-  my $s = $switches;
-  $s .= q[ "-T"] if $first =~ /^#!.*\bperl.*-\w*T/;
+    $fh->open($test) or print "can't open $test. $!\n";
+    my $first = <$fh>;
+    my $s = $switches;
+    $s .= q[ "-T"] if $first =~ /^#!.*\bperl.*-\w*T/;
     $fh->close or print "can't close $test. $!\n";
-  my $cmd = "$^X $s $test|";
-  $cmd = "MCR $cmd" if $^O eq 'VMS';
-  $fh->open($cmd) or print "can't run $test. $!\n";
-  for my $line (<$fh>) {
-    if( $verbose ){ print $line; }
-    if ($line=~/^1\.\.([0-9]+)/) {
-	  # Not supported in Result - It's needed!!!
-      #$result->plan($1);
-      $next=1;
-      $max=$1;
-	  $message="";
-    } elsif ($max && $line=~/^(not\s+)?ok\b/) {
-      my $this = $next;
-      if ($line=~/^not ok\s*(\d*)/){
-		$this = $1 if $1 > 0;
-		my $testcase=new Test::Unit::TestCase("$test case $this");
-		$result->start_test($testcase);
-		$result->
-		  add_failure(
-					  Test::Unit::UnitHarness::TestCase
-					  ->new("$test case $this"),
-					  Test::Unit::UnitHarness::Exception
-					  ->new($message));
-		$result->end_test($testcase);
-		$message="";
-      } elsif ($line=~/^ok\s*(\d*)/) {
-		$this = $1 if defined $1 and $1 ne "" and $1 > 0;
-		my $testcase=Test::Unit::UnitHarness::TestCase
-		  ->new("$test case $this");
-		$result->start_test($testcase);
-		$result->add_pass($testcase);
-		$result->end_test($testcase);
-		$message="";
-      }
-      $next++;
-    } else {
-	  # this is the message, not the medium...
-	  # this wasnt part of the Test::Harness protocol, so it
-	  # must be output from the program. Collect this, it might
-	  # prove useful!
-	  $message.=$line;
-	}
-  }
-  $fh->close; # must close to reap child resource values
-  if ($^O eq 'VMS') {
-    if (defined $old5lib) {
-      $ENV{PERL5LIB} = $old5lib;
-    } else {
-      delete $ENV{PERL5LIB};
+    my $cmd = "$^X $s $test|";
+    $cmd = "MCR $cmd" if $^O eq 'VMS';
+    $fh->open($cmd) or print "can't run $test. $!\n";
+    for my $line (<$fh>) {
+        print $line if $verbose;
+
+        if ($line =~ /^1\.\.([0-9]+)/) {
+            # Not supported in Result - It's needed!!!
+            #$result->plan($1);
+            $next = 1;
+            $max = $1;
+            $message = "";
+        }
+        elsif ($max && $line =~ /^(not\s+)?ok\b/) {
+            my $this = $next;
+            if ($line =~ /^not ok\s*(\d*)/) {
+                $this = $1 if $1 > 0;
+                my $testcase = new Test::Unit::TestCase("$test case $this");
+                $result->start_test($testcase);
+                $result->add_failure(
+                    Test::Unit::UnitHarness::TestCase->new("$test case $this"),
+                    Test::Unit::UnitHarness::Exception->new($message)
+                );
+                $result->end_test($testcase);
+                $message = "";
+            }
+            elsif ($line =~ /^ok\s*(\d*)/) {
+                $this = $1 if $1;
+                my $testcase =
+                  Test::Unit::UnitHarness::TestCase->new("$test case $this");
+                $result->start_test($testcase);
+                $result->add_pass($testcase);
+                $result->end_test($testcase);
+                $message = "";
+            }
+            $next++;
+        }
+        else {
+            # this is the message, not the medium...
+            # this wasnt part of the Test::Harness protocol, so it
+            # must be output from the program. Collect this, it might
+            # prove useful!
+            $message .= $line;
+        }
     }
-  }
+    $fh->close;             # must close to reap child resource values
+    if ($^O eq 'VMS') {
+        if (defined $old5lib) {
+            $ENV{PERL5LIB} = $old5lib;
+        } else {
+            delete $ENV{PERL5LIB};
+        }
+    }
 }
 
 sub name {
@@ -150,10 +150,11 @@ sub to_string {
 sub warning {
     my $self = shift;
     my ($message) = @_;
-	return Class::Inner->new(
-		parent => 'Test::Unit::TestCase',
-		methods => { run_test => sub { (shift)->fail($message) } },
-	    args => ['warning']);
+    return Class::Inner->new(
+        parent => 'Test::Unit::TestCase',
+        methods => { run_test => sub { (shift)->fail($message) } },
+        args => ['warning']
+    );
 }
 
 package Test::Unit::UnitHarness::TestCase;
@@ -163,7 +164,7 @@ sub run_test {
     my $self = shift;
     my $class = ref($self);
     my $method = $self->name();
-	$self->fail("This test is not restartable");
+    $self->fail("This test is not restartable");
 }
 
 package Test::Unit::UnitHarness::Exception;
@@ -173,11 +174,11 @@ use strict;
 sub new {
     my $class = shift;
     my ($message) = @_;
-	my $stacktrace = '';
+    my $stacktrace = '';
     
     $message = '' unless defined($message);
     $stacktrace = $class . ": Output from external test\n" 
-	  . $message . "\n";
+                         . $message . "\n";
     
     bless { stacktrace => $stacktrace }, $class;
 }
@@ -186,7 +187,9 @@ sub stacktrace {
     my $self = shift;
     return $self->{stacktrace};
 }
+
 1;
+
 __END__
 
 =head1 NAME
