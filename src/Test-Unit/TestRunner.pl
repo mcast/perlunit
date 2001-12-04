b4d97099b982757cda4d14d6b0708205f6be5b5c
adamspiers <adamspiers>
2001-12-04 14:29:21 +0000

    example usage of debugging

--- src/Test-Unit/TestRunner.pl
+++ src/Test-Unit/TestRunner.pl
@@ -2,7 +2,11 @@
 
 use strict;
 
+use Test::Unit::Debug qw(debug_pkgs);
 use Test::Unit::TestRunner;
+
+# Uncomment and edit to debug individual packages.
+#debug_pkgs(qw/Test::Unit::TestCase/);
 
 my $testrunner = Test::Unit::TestRunner->new();
 $testrunner->start(@ARGV);
