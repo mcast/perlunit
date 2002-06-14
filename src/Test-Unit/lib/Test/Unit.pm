ed2cebe0f7148bbc67478a4aa1f254ad281b6df4
adamspiers <adamspiers>
2002-06-14 10:40:48 +0000

    version 0.24

--- src/Test-Unit/lib/Test/Unit.pm
+++ src/Test-Unit/lib/Test/Unit.pm
@@ -76,7 +76,7 @@ use vars qw($VERSION);
 # this module if you say "install Test::Unit" in the CPAN
 # shell.  "make tardist" should do this automatically.
 
-$VERSION = '0.23';
+$VERSION = '0.24';
 
 1;
 __END__
